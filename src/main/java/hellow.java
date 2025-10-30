cf74bcae299745eb2034794f3fb18adf2a863b1b
kiser1214 <1503916463@qq.com>
2025-10-31 00:39:42 +0800

    Update hellow.java

--- src/main/java/hellow.java
+++ src/main/java/hellow.java
@@ -9,6 +9,15 @@ public class hellow {
         System.out.println("zzzzzzzzzzzz");
         System.out.println("zzzzzzzzzzzz");
         System.out.println("zzzzzzzzzzzz");
+        System.out.println("zzzzzzzzzzzz");
+        System.out.println("zzzzzzzzzzzz");
+        System.out.println("zzzzzzzzzzzz");
+        System.out.println("zzzzzzzzzzzz");
+        System.out.println("zzzzzzzzzzzz");
+        System.out.println("zzzzzzzzzzzz");
+        System.out.println("zzzzzzzzzzzz");
+        System.out.println("zzzzzzzzzzzz");
+        
 
     }
 }
